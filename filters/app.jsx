3fd6fb7a9814276c7bccc099bf3fc52cb9c07956
ershov-ilya <ershov.ilya@gmail.com>
2015-09-24 17:57:54 +0300

    filters building

--- filters/app.jsx
+++ filters/app.jsx
@@ -12,21 +12,42 @@
 
 var Select = React.createClass({
     getInitialState: function() {
+        switch(this.props.name){
+            case 'theme':
+                items=['managment', 'leadership'];
+                break;
+            case 'month':
+                items=['jan', 'feb', 'mar'];
+                break;
+            case 'year':
+                items=['2015','2016'];
+                break;
+        }
+
         return {
-            id:'filter-'+this.props.name
+            id:'filter-'+this.props.name,
+            items:items
         };
     },
 
+    componentDidMount: function() {
+        var $this=$(this);
+        $this.addClass('test');
+    },
+
     render: function() {
+        var classBlock='filter-select col-xs-4',
+            classSelect='form-control';
+
         return (
-            <div class="filter-select">
+            <div className={classBlock}>
                 <label>{this.state.id}</label>
-                <select id="{this.state.id}" class="form-control">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
+                <select id={this.state.id} className={classSelect}>
+                    {
+                        this.state.items.map(function(item) {
+                            return <option>{item}</option>
+                        })
+                    }
                 </select>
             </div>
         );
@@ -62,20 +83,34 @@ var App = React.createClass({
     },
 
     render: function() {
-        //console.log(this.state.items);
+        var filterClass='',
+            rowClass='row',
+            btnClass='btn btn-primary';
+
         return (
-            <div id="SmartFilter">
-                <Select name="theme" />
-                <Select name="month" />
-                <Select name="year" />
-                <ul id="List">
-                    {
-                        this.state.items.map(function(item) {
-                            return <li>{item.title} {item.description} {item.img}</li>
-                        })
-                    }
-                </ul>
-                <button id="LoadMore" onClick={this.LoadMore}>Load More</button>
+            <div id="SmartFilter" className={filterClass}>
+                <div className={rowClass}>
+                    <Select name="theme" />
+                    <Select name="month" />
+                    <Select name="year" />
+                </div>
+                <div className={rowClass}>
+                    <ul id="List">
+                        {
+                            this.state.items.map(function(item) {
+                                return <li>{item.title} {item.description} {item.img}</li>
+                            })
+                        }
+                    </ul>
+                </div>
+                <div className={rowClass}>
+                    <div className='col-xs-12 text-center'>
+                        <button id="LoadMore" className={btnClass+' btn-lg'} onClick={this.LoadMore}>
+                            <span className="glyphicon glyphicon-refresh" aria-hidden="true"></span>&nbsp;
+                             Load More
+                        </button>
+                    </div>
+                </div>
             </div>
         );
     }
